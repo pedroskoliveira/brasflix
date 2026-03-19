b408e5039af04de9d4005c21c42cea67206d2809
pedroskoliveira <161898333+pedroskoliveira@users.noreply.github.com>
2026-03-18 23:26:27 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { resolve } from "node:path";
 
 export default defineConfig({
   server: {
@@ -7,6 +8,23 @@ export default defineConfig({
   },
   build: {
     target: "esnext",
-    minify: "terser"
+    minify: "terser",
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "login.html"),
+        perfil: resolve(__dirname, "perfil.html"),
+        analytics: resolve(__dirname, "analytics.html"),
+        face: resolve(__dirname, "face.html"),
+        video: resolve(__dirname, "video.html"),
+        adminDashboard: resolve(__dirname, "admin/dashboard.html"),
+        adminVideos: resolve(__dirname, "admin/videos.html"),
+        adminCategorias: resolve(__dirname, "admin/categorias.html"),
+        adminComentarios: resolve(__dirname, "admin/comentarios.html"),
+        adminAnalytics: resolve(__dirname, "admin/analytics.html"),
+        adminSetter: resolve(__dirname, "admin-setter.html"),
+        setupAdmin: resolve(__dirname, "setup-admin.html")
+      }
+    }
   }
 });
